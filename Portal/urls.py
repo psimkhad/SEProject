ff04c5c5f647d8d478e7d8074f1ae9c5ff8712e1
pkc-1199 <70533673+pkc-1199@users.noreply.github.com>
2020-11-23 00:40:34 -0600

    Implemented urls.py

---
+++ Portal/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+from django.contrib.auth.views import LoginView
+
+urlpatterns = [
+    path('', views.login),
+    path('login', views.login),
+    path('link', views.link),
+    path('help', views.help),
+    path('logout', views.logout)
+]
