ef3c49bcd4f86bf8014db43f607bccd9566d021d
psimkhad <55363132+psimkhad@users.noreply.github.com>
2020-11-23 02:04:43 -0600

    Implemented user.sql
    Created username, password and user role in the database table

---
+++ Scripts/User.sql
@@ -0,0 +1,7 @@
+insert into portal_user (username, password, userrole) values('superuser',  'admin123', 'SUPERUSER');
+insert into portal_user (username, password, userrole) values('supportuser',  'test123', 'ADMIN');
+insert into portal_user (username, password, userrole) values('financeuser',  'test123', 'FINANCE_ADMIN');
+insert into portal_user (username, password, userrole) values('salesuser',  'test123', 'SALES_ADMIN');
+insert into portal_user (username, password, userrole) values('hruser',  'test123', 'HR_ADMIN');
+insert into portal_user (username, password, userrole) values('techuser',  'test123', 'TECH_ADMIN');
+commit;
