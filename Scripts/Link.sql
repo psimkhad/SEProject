f1c24907959bad452a47f288512cc58a13e17713
psimkhad <55363132+psimkhad@users.noreply.github.com>
2020-11-23 02:02:30 -0600

    sql code implemented

---
+++ Scripts/Link.sql
@@ -0,0 +1,25 @@
+insert into portal_link (userrole, url, category, appname) values('ADMIN', 'https://localhost/manageaccount', 'Support', 'Manage User Accounts');
+insert into portal_link (userrole, url, category, appname) values('ADMIN', 'https://localhost/assignrole', 'Support', 'Assign Roles');
+insert into portal_link (userrole, url, category, appname) values('ADMIN', 'https://localhost/helpdesk', 'Support', 'Help Desk');
+
+insert into portal_link (userrole, url, category, appname) values('FINANCE_ADMIN', 'https://localhost/financereport', 'Finance', 'Finance Reports');
+insert into portal_link (userrole, url, category, appname) values('FINANCE_ADMIN', 'https://localhost/accountpayable', 'Finance', 'Accounts Payable');
+insert into portal_link (userrole, url, category, appname) values('FINANCE_ADMIN', 'https://localhost/accountreceivable', 'Finance', 'Accounts Receivable');
+insert into portal_link (userrole, url, category, appname) values('FINANCE_ADMIN', 'https://localhost/tax', 'Finance', 'Tax');
+
+insert into portal_link (userrole, url, category, appname) values('SALES_ADMIN', 'https://localhost/salesreport', 'Sales', 'Sales Reports');
+insert into portal_link (userrole, url, category, appname) values('SALES_ADMIN', 'https://localhost/saleslead', 'Sales', 'Sales Leads');
+insert into portal_link (userrole, url, category, appname) values('SALES_ADMIN', 'https://localhost/salesdemo', 'Sales', 'Sales Demo');
+
+insert into portal_link (userrole, url, category, appname) values('HR_ADMIN', 'https://localhost/newhire', 'HR', 'New Hire On-boarding');
+insert into portal_link (userrole, url, category, appname) values('HR_ADMIN', 'https://localhost/benefit', 'HR', 'Benefits');
+insert into portal_link (userrole, url, category, appname) values('HR_ADMIN', 'https://localhost/payroll', 'HR', 'Payroll');
+insert into portal_link (userrole, url, category, appname) values('HR_ADMIN', 'https://localhost/offboard', 'HR', 'Off-boarding');
+insert into portal_link (userrole, url, category, appname) values('HR_ADMIN', 'https://localhost/hrreport', 'HR', 'HR Reports');
+
+insert into portal_link (userrole, url, category, appname) values('TECH_ADMIN', 'https://localhost/appmonitor', 'Technology', 'Application Monitoring');
+insert into portal_link (userrole, url, category, appname) values('TECH_ADMIN', 'https://localhost/techsupport', 'Technology', 'Tech Support');
+insert into portal_link (userrole, url, category, appname) values('TECH_ADMIN', 'https://localhost/appdev', 'Technology', 'App Development');
+insert into portal_link (userrole, url, category, appname) values('TECH_ADMIN', 'https://localhost/appadmin', 'Technology', 'App Admin');
+insert into portal_link (userrole, url, category, appname) values('TECH_ADMIN', 'https://localhost/releasemanage', 'Technology', 'Release Management');
+commit;
