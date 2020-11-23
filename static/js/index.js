e018482fbd4f6b9e19192ab42f13380474e6756a
psimkhad <55363132+psimkhad@users.noreply.github.com>
2020-11-23 02:40:09 -0600

    Static folder added
    Image libraries and css folder added

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+    $('#example').DataTable();
+} );
+
+// var f_makeActive = function (e) {
+//     $(e).preventDefault()
+//     $('.navlink').removeClass('active')
+//     $(e).parent().addClass('active')
+// }
