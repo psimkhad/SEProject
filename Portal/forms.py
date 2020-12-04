9ac938a9474f075bb59bb01a2e7efe436b976ee6
psimkhad <55363132+psimkhad@users.noreply.github.com>
2020-12-04 02:24:44 -0600

    Updated forms.py

--- Portal/forms.py
+++ Portal/forms.py
@@ -7,3 +7,26 @@ class LoginForm(forms.Form):
 
     password = forms.CharField(required=True, label='',
                                widget=forms.PasswordInput(attrs={'placeholder': 'Passsword', 'class': 'form-control'}))
+
+
+class RegisterForm(forms.Form):
+
+    ROLES = [
+        ('ADMIN', 'Admin'),
+        ('FINANCE_ADMIN', 'Finance Admin'),
+        ('SALES_ADMIN', 'Sales Admin'),
+        ('HR_ADMIN', 'HR Admin'),
+        ('TECH_ADMIN', 'Tech Admin'),
+    ]
+
+    username = forms.CharField(required=True, label='',
+                               widget=forms.TextInput(attrs={'placeholder': 'User Name', 'class': 'form-control'}))
+
+    userrole = forms.CharField(required=True, label='',
+                               widget=forms.Select(choices=ROLES, attrs={'class': 'form-control'}))
+
+    password = forms.CharField(required=True, label='',
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Passsword', 'class': 'form-control'}))
+
+    confirm_password = forms.CharField(required=True, label='',
+                                       widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Passsword', 'class': 'form-control'}))
