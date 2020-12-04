11e9b56bcf7071afe3129661b2560e7debf45a54
psimkhad <55363132+psimkhad@users.noreply.github.com>
2020-12-04 02:40:18 -0600

    Update urls.py

--- Portal/urls.py
+++ Portal/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('', views.login),
     path('login', views.login),
+    path('register', views.register),
     path('link', views.link),
     path('help', views.help),
     path('logout', views.logout)
